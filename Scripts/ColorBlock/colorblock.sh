d128de9502ad8c018ad6589933d236dc945f275a
Anurag Roy <anuragroy11@gmail.com>
2018-03-02 12:21:57 +0530

    Update colorblock.sh

--- Scripts/ColorBlock/colorblock.sh
+++ Scripts/ColorBlock/colorblock.sh
@@ -5,11 +5,11 @@
 echo
 echo "       Color Scheme: Oceanic Future       " #Hard-coded
 echo
-Width='      '   # Adjust the width of the blocks by adding or reducing spaces in W
+Width='      '     # Adjust the width of the blocks by adding or reducing spaces in Width
 
-for Height in 1 2 3  #Adjust the height of the blocks by adding more numbers
+for Height in 1 2 3      #Adjust the height of the blocks by adding more numbers
   do Heighter=${Height// /}
-  for BG in 43m 41m 45m 44m 46m 42m  #Background colors 
+  for BG in 43m 41m 45m 44m 46m 42m    #Background colors 
     do echo -en " \033[$BG$Width\033[0m"
   done
   echo
@@ -21,6 +21,6 @@ echo
 #42m is Green
 #43m is Yellow
 #44m is Blue
-#45m is Pinks
+#45m is Pink
 #46m is Cyan
 #47m is White
